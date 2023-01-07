5cb9dea0846cfb78f4a920a9166f0883b2e173f1
Artem Levin <leoartemlev@yandex.ru>
2023-01-07 13:45:01 +0500

    added password binding

--- RestaurantAccounting.WPF/Pages/AuthPage.xaml.cs
+++ RestaurantAccounting.WPF/Pages/AuthPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using MvvmCross.Platforms.Wpf.Presenters.Attributes;
+﻿using System.Windows;
+using System.Windows.Controls;
+using MvvmCross.Platforms.Wpf.Presenters.Attributes;
 using MvvmCross.Platforms.Wpf.Views;
 using MvvmCross.ViewModels;
 using RestaurantAccounting.ViewModels;
@@ -12,5 +14,11 @@ public partial class AuthPage : MvxWpfView
     public AuthPage()
     {
         InitializeComponent();
+    }
+
+    private void OnPasswordChanged(object sender, RoutedEventArgs e)
+    {
+        var actualPassword = (sender as PasswordBox)?.Password;
+        if (ViewModel is AuthViewModel authViewModel) authViewModel.Password = actualPassword ?? "";
     }
 }
