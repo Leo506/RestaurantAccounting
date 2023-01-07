d485b8d3214f9e8634776b939805d0818bccb105
Artem Levin <leoartemlev@yandex.ru>
2023-01-07 13:28:10 +0500

    fix auth view

--- RestaurantAccounting.WPF/Pages/AuthPage.xaml.cs
+++ RestaurantAccounting.WPF/Pages/AuthPage.xaml.cs
@@ -1,9 +1,11 @@
-﻿using MvvmCross.Platforms.Wpf.Views;
+﻿using MvvmCross.Platforms.Wpf.Presenters.Attributes;
+using MvvmCross.Platforms.Wpf.Views;
 using MvvmCross.ViewModels;
 using RestaurantAccounting.ViewModels;
 
 namespace RestaurantAccounting.WPF.Pages;
 
+[MvxContentPresentation]
 [MvxViewFor(typeof(AuthViewModel))]
 public partial class AuthPage : MvxWpfView
 {
