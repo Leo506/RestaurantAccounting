0d18815f9a8e7e184438fc4bdc0bcf49340ce706
Artem Levin <leoartemlev@yandex.ru>
2023-01-07 13:24:33 +0500

    added Setup.cs

---
+++ RestaurantAccounting.WPF/Setup.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Logging;
+using MvvmCross.Platforms.Wpf.Core;
+using Serilog;
+using Serilog.Extensions.Logging;
+
+namespace RestaurantAccounting.WPF;
+
+public class Setup : MvxWpfSetup<RestaurantAccounting.App>
+{
+    protected override ILoggerProvider CreateLogProvider()
+    {
+        return new SerilogLoggerProvider();
+    }
+
+    protected override ILoggerFactory CreateLogFactory()
+    {
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .WriteTo.Console()
+            .CreateLogger();
+
+        return new SerilogLoggerFactory();
+    }
+}
