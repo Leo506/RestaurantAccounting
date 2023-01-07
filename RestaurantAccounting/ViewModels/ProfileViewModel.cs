5a2ff2500ffa11dff6021fca104d8cb912d493d6
Artem Levin <leoartemlev@yandex.ru>
2023-01-07 15:33:28 +0500

    create command for opening personal shift and closing it; added changing button text

--- RestaurantAccounting/ViewModels/ProfileViewModel.cs
+++ RestaurantAccounting/ViewModels/ProfileViewModel.cs
@@ -1,4 +1,6 @@
-﻿using MvvmCross.ViewModels;
+﻿using System.Windows.Input;
+using MvvmCross.Commands;
+using MvvmCross.ViewModels;
 using RestaurantAccounting.Models;
 
 namespace RestaurantAccounting.ViewModels;
@@ -11,7 +13,36 @@ public class ProfileViewModel : MvxViewModel<UserModel>
 
     public string LastName => _userModel.LastName;
 
-    public bool IsPersonalShiftOpen => _userModel.IsPersonalShiftOpen;
+    public bool IsPersonalShiftOpen
+    {
+        get => _userModel.IsPersonalShiftOpen;
+        set
+        {
+            _userModel.IsPersonalShiftOpen = value;
+            RaisePropertyChanged(() => IsPersonalShiftOpen);
+        }
+    }
+
+    private string? _openShiftText = default!;
+    public string OpenShiftText
+    {
+        get => _openShiftText ??= IsPersonalShiftOpen ? "Close shift" : "Open shift";
+        set
+        {
+            _openShiftText = value;
+            RaisePropertyChanged(() => OpenShiftText);
+        }
+    }
+
+    #region OpenShiftCommand
+    private MvxCommand? _openShiftCommand;
+    public ICommand OpenShiftCommand =>
+        _openShiftCommand ??= new MvxCommand(() =>
+        {
+            IsPersonalShiftOpen = !IsPersonalShiftOpen;
+            OpenShiftText = IsPersonalShiftOpen ? "Close shift" : "Open shift";
+        });
+    #endregion
     
     private UserModel _userModel = default!;
     
