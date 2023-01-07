96675de0a692bd2f0aac102b312ba52fee917898
Artem Levin <leoartemlev@yandex.ru>
2023-01-07 13:24:44 +0500

    added AuthPage

---
+++ RestaurantAccounting.WPF/Pages/AuthPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using MvvmCross.Platforms.Wpf.Views;
+using MvvmCross.ViewModels;
+using RestaurantAccounting.ViewModels;
+
+namespace RestaurantAccounting.WPF.Pages;
+
+[MvxViewFor(typeof(AuthViewModel))]
+public partial class AuthPage : MvxWpfView
+{
+    public AuthPage()
+    {
+        InitializeComponent();
+    }
+}
