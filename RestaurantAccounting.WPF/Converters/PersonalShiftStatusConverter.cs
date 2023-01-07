db1a5a43092c6a810c25c9c1d4bb2770873e4210
Artem Levin <leoartemlev@yandex.ru>
2023-01-07 14:31:12 +0500

    create converter for personal shift status

---
+++ RestaurantAccounting.WPF/Converters/PersonalShiftStatusConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace RestaurantAccounting.WPF.Converters;
+
+[ValueConversion(typeof(bool), typeof(string))]
+public class PersonalShiftStatusConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        var shiftStatus = (bool)value!;
+        return shiftStatus ? "Open" : "Closed";
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
