c0c81de1f942fea60950fad5a7168107cc82a977
Artem Levin <leoartemlev@yandex.ru>
2023-01-15 12:38:42 +0500

    created RegistrationPage

---
+++ RestaurantAccounting.WPF/Pages/RegistrationPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using MvvmCross.Platforms.Wpf.Views;
+using MvvmCross.ViewModels;
+using RestaurantAccounting.Core.ViewModels;
+
+namespace RestaurantAccounting.WPF.Pages;
+
+[MvxViewFor(typeof(RegistrationViewModel))]
+public partial class RegistrationPage : MvxWpfView<RegistrationViewModel>
+{
+    public RegistrationPage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnPasswordChanged(object sender, RoutedEventArgs e)
+    {
+        ViewModel.Password = (sender as PasswordBox)!.Password;
+    }
+}
