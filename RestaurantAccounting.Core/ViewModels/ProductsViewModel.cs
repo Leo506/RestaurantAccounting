082e22a3ed1f88cda67eaa461d15853b5dc4c6ee
Artem Levin <leoartemlev@yandex.ru>
2023-01-21 17:16:01 +0500

    added back button

--- RestaurantAccounting.Core/ViewModels/ProductsViewModel.cs
+++ RestaurantAccounting.Core/ViewModels/ProductsViewModel.cs
@@ -1,4 +1,7 @@
-﻿using MvvmCross.ViewModels;
+﻿using System.Windows.Input;
+using MvvmCross.Commands;
+using MvvmCross.Navigation;
+using MvvmCross.ViewModels;
 using RestaurantAccounting.Core.Models;
 using RestaurantAccounting.Core.Services.ProductService;
 
@@ -6,13 +9,18 @@ namespace RestaurantAccounting.Core.ViewModels;
 
 public class ProductsViewModel : MvxViewModel
 {
+    private MvxCommand? _backCommand;
+    public ICommand BackCommand => _backCommand ??= new MvxCommand(() => _navigationService.Close(this));
+    
     public MvxObservableCollection<Product> Products { get; private set; }
 
     private readonly IProductService _productService;
-    
-    public ProductsViewModel(IProductService productService)
+    private readonly IMvxNavigationService _navigationService;
+
+    public ProductsViewModel(IProductService productService, IMvxNavigationService navigationService)
     {
         _productService = productService;
+        _navigationService = navigationService;
         var tmp = _productService.GetAll();
         Products = new MvxObservableCollection<Product>(_productService.GetAll());
     }
