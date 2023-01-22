cb2bb6df69109406d71a0a3985ffd0bf19533a62
Artem Levin <leoartemlev@yandex.ru>
2023-01-22 14:13:47 +0500

    create interface and class for alerts

---
+++ RestaurantAccounting.Core/Interactions/IAlertInteraction.cs
@@ -0,0 +1,13 @@
+﻿namespace RestaurantAccounting.Core.Interactions;
+
+public enum AlertType
+{
+    None,
+    Error,
+    Warning
+}
+
+public interface IAlertInteraction
+{
+    public void AlertAsync(string message, string title = "", AlertType alertType = AlertType.None);
+}
